418ce81f925c467a84260bbe9dd94d90b67401dd
Jenny Lin <Jenny13TS13@gmail.com>
2015-03-04 23:39:18 -0500

    Added Emitter Stuff

--- proj2/include/graphics.h
+++ proj2/include/graphics.h
@@ -1,24 +1,42 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
-typedef struct {
+typedef struct{
   float loc[3]; //x,y,z coordinates
-  float alpha; //alpha value
-  float color[3]; //color of particle
-  vec3 dir; //direction of movement     
+  float color[4]; //r,g,b,alpha
+  //vec3 dir; //direction of movement     
   int life; //lifetime left
   float speed[3]; //velocity
 } Particle;
 
+typedef struct{
+  float loc[3]; //x,y,z coordinates
+  int pSize; //number of active particles
+  Particle *pList; //list of particles emitted
+  int maxSize; //maximum capacity of particles
+} Emitter;
 
+/* Particle */
 Particle *particle_create( void );
-Particle *particle_createp(float *location, float a, float *c, 
-			 vec3 direction, int l, float *velocity);
+Particle *particle_createp(float *location, float *c, 
+			   /*vec3 direction,*/ int l, float *velocity);
 void particle_free(Particle *p);
 
 void particle_init(Particle *p);
-void particle_set(Particle *p, float *location, float a, float *c, 
-		  vec3 direction, int l, float *velocity);
+void particle_set(Particle *p, float *location, float *c, 
+		  /*vec3 direction,*/ int l, float *velocity);
 void particle_copy(Particle *to, Particle *from);
 void particle_print(Particle *p, FILE*fp);
-  
+
+/* Emitter */
+Emitter *emitter_create( void );
+void emitter_init( Emitter *e );
+void emitter_set( Emitter *e, float *loc, int pSize);
+void emitter_clear( Emitter *e );
+void emitter_free( Emitter *e );
+
+#endif
