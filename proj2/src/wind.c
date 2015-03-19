5788d03d719187fdba85e46e9561f716fd171778
Jenny Lin <Jenny13TS13@gmail.com>
2015-03-19 00:02:25 -0400

    Added Scene w/ wind

--- proj2/src/wind.c
+++ proj2/src/wind.c
@@ -8,9 +8,31 @@
 
 
 Emitter *e;
-Obstacle * oList[2];
-Obstacle *o1, *o2;
 Wind *w;
+
+void drawCircle(float x, float y, float z, float r, int slices){
+    int i;
+    
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex3f( x, y, z);
+    for(i=0; i<=slices; i++){
+        glVertex3f( x + (r * cos(i * 2.0f*M_PI / slices)),
+                   y,
+                   z + (r * sin(i * 2.0f*M_PI / slices)));
+    }
+    glEnd();
+}
+
+void drawCylinder( float r, float h, int slices){
+    GLUquadricObj *quadObj = gluNewQuadric();
+    glPushMatrix();
+    glRotatef( 90.0, 1.0, 0.0, 0.0);
+    gluCylinder(quadObj, r, r, h, slices, 1);
+    glPopMatrix();
+    drawCircle( 0, 0, 0, r, slices );
+    drawCircle( 0, -h, 0, r, slices );
+}
+
 
 // Function for drawing the contents of the screen
 void display(void) {
@@ -25,15 +47,16 @@ void display(void) {
     glLoadIdentity();
     
     // set up the viewing transformation
-    gluLookAt(0.0, 1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    gluLookAt(0.0, 0.5, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     
     // set up the light
     glLightfv(GL_LIGHT0, GL_POSITION, position);
     
-    emitter_update(e, oList, w, 2);
+    emitter_update(e, NULL, w, 0);
     emitter_draw(e);
-    obstacle_draw(oList, 2);
-    // draw everything
+    
+    drawCylinder( 0.1f, 2.0f, 30 );
+    //draw everything
     glFlush();
     usleep(10000);
     glutPostRedisplay();
@@ -65,10 +88,8 @@ void reshape(int w, int h) {
 // init function
 void init(void) {
     float loc[3] = {0.0, 0.0, 0.0};
-    float coord1[6] = { -0.37, 0.0, 0.3, 0.3, -0.2, 0.2 };
-    float coord2[6] = { 0.0, 0.37, 0.15, 0.15, -0.2, 0.2 };
-    float speed[3] = {0.0000, 0, 0.0};//{0.2,0.0,0.3};
-    float wloc[3] = {0.06, 0.5, 0.0};//{0.05,0.2,0};
+    float speed[3] = {0.0003, 0, 0.0};//{0.2,0.0,0.3};
+    float wloc[3] = {0.06, 0.7, 0.0};//{0.05,0.2,0};
     // background color
     glClearColor(0.0, 0.0, 0.0, 0.0);
     
@@ -77,16 +98,8 @@ void init(void) {
     
     //initlights();
     e = emitter_create();
-    o1 = obstacle_create();
-    o2 = obstacle_create();
-    
-    
     emitter_set(e, loc, 500000);
     emitter_setup( e, 500 );
-    obstacle_set(o1, coord1, 0 );
-    obstacle_set(o2, coord2, 0 );
-    oList[0] = o1;
-    oList[1] = o2;
     
     w = wind_create();
     wind_set(w, wloc, speed);
@@ -101,8 +114,6 @@ void keyboard(unsigned char key, int x, int y){
     switch( key) {
         case 'q': // quit
             emitter_free(e);
-            obstacle_free(o1);
-            obstacle_free(o2);
             wind_free(w);
             exit(0);
             break;
@@ -119,7 +130,7 @@ int main(int argc, char **argv) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Collision Checking");
+    glutCreateWindow("Wind");
     init();
     
     // set up the callback functions
