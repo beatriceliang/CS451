22a9acc715df3009067fe9494f6c3e5105c58971
Jenny Lin <Jenny13TS13@gmail.com>
2015-04-10 23:29:40 -0400

    Grayscaled ascii

--- proj3/src/con.c
+++ proj3/src/con.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include "graphics.h"
 
 int main(int argc, char *argv[]) {
+  /* Mass converting between ppm and png */
+  /*  
   int i;
   char *s = malloc(strlen("convert 111.png 111.ppm "));
   
@@ -12,4 +15,33 @@ int main(int argc, char *argv[]) {
     system(s);
   }
   free(s);
+  */
+
+
+  /* Grayscaling ascii */
+  int i, j, k;
+  CharSet *c;
+  char *s = malloc(strlen("character/111.ppm "));
+  float intensity;
+
+  c = set_create(1);
+  char_read(c);
+  //set_intensity(c);
+  for( i=0; i<c->size; i++ ){
+    for(j = 0; j< c->chars[i]->image->rows; j++){
+      for(k = 0; k < c->chars[i]->image->cols; k++){
+	intensity = (c->chars[i]->image->data[j][k].rgb[0] + 
+		     c->chars[i]->image->data[j][k].rgb[1] +
+		     c->chars[i]->image->data[j][k].rgb[2])/3;
+
+	c->chars[i]->image->data[j][k].rgb[0] = intensity;
+	c->chars[i]->image->data[j][k].rgb[1] = intensity;
+	c->chars[i]->image->data[j][k].rgb[2] = intensity;
+      }
+    }
+    sprintf(s, "character/%d.ppm", i+32);
+    
+    image_write(c->chars[i]->image, s);
+  }
+
 }
